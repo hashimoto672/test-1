87c0ba22536f6ba08063330385c58d70b0787953
ouiu8 <rruru9916@gmail.com>
2019-06-24 20:59:17 +0900

    java1日目

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//Hello World!の出力
+		float a1 = (float)10/3;
+		System.out.println(a1);
+
+		double a2 = (double)10/3;
+		System.out.println(a2);
+
+		float a3 = 5/2;
+		System.out.println(a3);
+
+		float a4 = (float)5/2;
+		System.out.println(a4);
+
+		System.out.println(--a1);
+		System.out.println(--a1);
+	}
+
+}
