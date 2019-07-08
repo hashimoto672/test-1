a0a80341e09cd782b7ee0b14b0606c93aeafb94b
ouiu8 <rruru9916@gmail.com>
2019-07-08 14:10:13 +0900

    javaとデータベース接続

--- TestDB/src/CheckLoadJDBC.java
+++ TestDB/src/CheckLoadJDBC.java
@@ -6,13 +6,15 @@ public class CheckLoadJDBC {
 		// TODO 自動生成されたメソッド・スタブ
 		String msg = "";
 
-		try {
+		try{
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			msg="ドライバのロードに成功しました";
+			msg = "ドライバのロードに成功しました";
 		}catch(ClassNotFoundException e){
-			msg="ドライバのロードに失敗しました";
+			msg = "ドライバのロードに失敗しました";
 		}
 		System.out.println(msg);
+
+
 	}
 
 }
